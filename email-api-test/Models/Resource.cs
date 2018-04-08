4a0dbdea4ec0743865b011b07a86d707777a781a
Randell Luangrath <rbl2n@mtmail.mtsu.edu>
2018-04-07 19:15:38 -0500

    added resource model

---
+++ email-api-test/Models/Resource.cs
@@ -0,0 +1,27 @@
+﻿
+namespace email_api_test.Models
+{
+    public class Resource
+    {
+        public string Sid { get; set; }
+        public string ParentCallSid { get; set; }
+        public string DateCreated { get; set; }
+        public string DateUpdated { get; set; }
+        public string AccountSid { get; set; }
+        public string To { get; set; }
+        public string From { get; set; }
+        public string PhoneNumberSid { get; set; }
+        public string Status { get; set; }
+        public string StarTime { get; set; }
+        public string EndTime { get; set; }
+        public string Duration { get; set; }
+        public string Price { get; set; }
+        public string PriceUnit { get; set; }
+        public string Direction { get; set; }
+        public string AnsweredBy { get; set; }
+        public string ForwardedFrom { get; set; }
+        public string ToFormatted { get; set; }
+        public string CallerName { get; set; }
+        public string Uri { get; set; }
+    }
+}
